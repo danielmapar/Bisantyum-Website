a1b73d981eb818433071b89240473bc1db829d53
Daniel Marchena Parreira <daniel.parreira@udoctor.com.br>
2015-02-05 16:14:19 -0500

    Site update

--- mail.php
+++ mail.php
@@ -6,15 +6,17 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'bisantyum@gmail.com';     // SMTP username
-$mail->Password = 'Dertyu765';                        // SMTP password
+$mail->Username = 'bisantyum@gmail.com';              // SMTP username
+$mail->Password = '';                        // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
 $mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
 
 $mail->From = $_POST['email'];
 $mail->FromName = $_POST['name'];
 
-$mail->addAddress('danielmapar@gmail.com', 'Daniel Parreira');     // Add a recipient                                 
+$mail->addAddress('danielmapar@gmail.com', 'Daniel Parreira');     // Add a recipient       
+$mail->addAddress('info@bisantyum.com', 'Bisantyum Info');     // Add a recipient    
+
  
 $mail->Subject = "BISANTYUM - Website Form";
 $mail->Body    = $_POST['name'] . "(" . $_POST['email'] . ")<br/>" . $_POST['message'];
